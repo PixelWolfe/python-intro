ff2d3c60c4f00c81873a89eb41867f9a4cb6678c
Rob Wolfe <robertejohnson10@gmail.com>
2020-08-18 14:42:28 -0500

    Added primitive experiment file

---
+++ primitives.py
@@ -0,0 +1,22 @@
+# Let's use the type function to determine primitive types in python
+
+# Integers! 
+type(1) 
+print("I'm an integer!")
+
+# Float!
+type(1.33)
+print("I'm a float!")
+
+# Adding Integers to Floats results in a float
+print(2 + 3.4) #returns 5.4
+
+# +  Addition
+# -  Subtraction
+# *  Multiplication
+# /  Division
+# ** Exponentiation
+# %  Modulo
+# // Integer Division
+
+print(3-1.0)
